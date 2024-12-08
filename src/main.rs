cb5697aa493d8e496eda1191ebfc6bb217bd20d7
Robert Starosta <robik2k4@gmail.com>
2024-12-08 23:51:11 +0100

    testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+extern crate uom;
+
+// use uom::si::{mole, molar_mass};
+use uom::num::One;
+use uom::si::f64::*;
+use uom::si::amount_of_substance;
+use uom::si::amount_of_substance::mole;
+
+// const MOLENUM : MoleNumber = AmountOfSubstance::new::<mole>;
+
+
 fn main() {
-    let Avogadro = 6.02214076;
+    const AVOGDRO_CONST: f64 = 6.02214076e23; //N↓A
+    const ATOM_NUM : f64 = 12.21;
 
-
-    println!("Hello, world!");
+    println!("{}", AVOGDRO_CONST);
 }
