1cb95716551fdb013403b7cfa09ad5de58ef7a7f
Robert Starosta <robik2k4@gmail.com>
2025-10-06 16:46:55 +0200

    forgot about modules.rs

---
+++ src/modules.rs
@@ -0,0 +1,9 @@
+// pub use periodic_lookup::*;
+// pub use bethe_formula::*;
+
+pub mod periodic_table {
+    pub mod mean_excitation_energies;
+    pub mod periodic_lookup;
+}
+
+pub mod bethe_formula;
